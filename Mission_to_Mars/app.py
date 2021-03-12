0b39935d29e7c965cc2ee5f5a47a52237f95f470
Esther Kim <74941139+estherkim0998@users.noreply.github.com>
2021-03-11 21:36:26 -0800

    completed files

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 
-# import scrape mars py file 
+import scrape_mars
 
 
 app = Flask(__name__)
@@ -11,12 +11,15 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 @app.route("/")
 def index():
-    return "Hello World"
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
 def scrape():
-   
-    return "Scrape is working"
+    mars = mongo.db.mars
+    mars_data = scrape_mars.scrape_all()
+    mars.update({},mars_data, upsert=True)
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,171 +1,140 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[98]:
-
-
-#import dependencies
-import pandas as pd
-from bs4 import BeautifulSoup as bs
+# import splinter beautifulSoup and pandas
 from splinter import Browser
-from pprint import pprint
-import pymongo
-import requests
+from bs4 import BeautifulSoup as soup
+import pandas as pd
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-# In[89]:
+def scrape_all():
+    # excutable_path 
+    executable_path = { "executable_path": ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    news_title, news_paragraph = mars_news(browser) 
 
-
-#executable_path
-
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-
-
-# In[90]:
-
-
-#visit the nasa mars site
-
-url = ('https://mars.nasa.gov/news/')
-browser.visit(url)
-html= browser.html
-soup = bs(html, 'html.parser')
-
-
-# In[91]:
-
-
-print(soup.prettify())
-
-
-# In[70]:
-
-
-# all titles and bodies
-titles = soup.find_all('div', class_="content_title")
-print(titles)
-body = soup.find_all('div', class_="article_teaser_body")
-print(body)
-
-
-# In[71]:
-
-
-# find first title and body
-slides = soup.find_all('li', class_='slide')
-
-title = slides[0].find('div', class_= 'content_title')
-ttext = title.text.strip()
-body = slides[0].find('div', class_= 'article_teaser_body')
-btext = body.text.strip()
-
-print(ttext)
-print(btext)
-
-
-# In[92]:
-
-
-#quit browser
-browser.quit
-
-
-# In[94]:
-
-
-#vist url
-url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-browser.visit(url)
-html = browser.html
-soup = bs(html, 'html.parser')
-
-
-# In[83]:
-
-
-#get image 
-images = soup.find_all('img', class_='headerimage fade-in')
-print(images)
-
-
-# In[86]:
-
-
-#get just src
-for image in images:
-   pic = (image['src'])
-print(pic)
-
-
-# In[87]:
-
-
-#get image url
-featured_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{pic}'
-print(featured_url)
-
-
-# In[100]:
-
-
-#Mars Fact
-df = pd.read_html("https://space-facts.com/mars/")[0]
-df.columns =["Description","Mars"]
-df.set_index("Description", inplace=True)
-df
-
-
-# In[101]:
-
-
-df.to_html()
-
-
-# In[112]:
-
-
-# Hemisphere
-url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
-browser.visit(url)
-
-
-# In[113]:
-
-
-hemisphere_image_urls = []
-# Fist get a list of all the hemisphere
-links = browser.find_by_css("a.product-item h3")
-
-
-for index in range(len(links)):
-    hemisphere = {}
-    browser.find_by_css("a.product-item h3")[index].click()
+    data = {
+     "new_title": news_title,
+     "news_paragraph": news_paragraph,
+     "featured_image": featured_image(browser),
+     "facts": mars_facts(),
+     "hemispheres": hemispheres(browser)
+        
+    }
+    browser.quit()
+    return data
     
-    #Next is find the sample image anchor tag and extract the href
-    sample_element = browser.links.find_by_text("Sample").first
-    title = browser.find_by_css("h2.title").text
-    link =sample_element["href"]
-    hemisphere["title"] = title
-    hemisphere["link"] = link
+
+# NASA Mars News
+def mars_news(browser):
+    # Visit the NASA mars news site
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+
+    html = browser.html
+    # Convert the browser html to a soup object and then quit the browser
+    new_soup = soup(html, 'html.parser')
     
-    hemisphere_image_urls.append(hemisphere)
-    print("Retrieve url and link")
-    browser.back()
+    try:
+        slide_element = new_soup.select_one("ul.item_list li.slide")
+        # news title 
+        news_title = slide_element.find("div", class_="content_title").get_text()
+        print(news_title)
+
+        new_paragraph = slide_element.find("div", class_="article_teaser_body").get_text()
+        print(new_paragraph)
+    except:
+        return None, None
+
+    return news_title, new_paragraph
+
+
+def featured_image(browser):
+    # # feature image
+    url = "https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html"
+    browser.visit(url)
+    html = browser.html
+    new_soup = soup(html, 'html.parser')
     
-print(hemisphere_image_urls)
+    try:
+        full_image_element = new_soup.find("a", class_="showimg fancybox-thumbs")
+    except:
+        return None
+    
+    image_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{full_image_element["href"]}'
+
+    return image_url
+    
+def mars_facts():
+    # # Mars Fact
+
+    df = pd.read_html("https://space-facts.com/mars/")[0]
+    df.columns=["Description", "Mars"]
+    df.set_index("Description", inplace=True)
+
+    return df.to_html(classes="table table-striped")
+
+def hemispheres(browser):
+    # # # Hemisphere 
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
 
 
-# In[114]:
+    hemisphere_image_urls = []
+
+    # First get a list of all the hemisphere
+    links = browser.find_by_css("a.product-item h3")
+
+    for index in range(len(links)):
+        hemisphere = {}
+
+        browser.find_by_css("a.product-item h3")[index].click()
+
+        # Next is find the sample image anchor tag and extract the href 
+        try: 
+            sample_element = browser.links.find_by_text("Sample").first
+            title = browser.find_by_css("h2.title").text
+            link = sample_element["href"]
+
+            hemisphere["title"] = title
+            hemisphere["link"] = link
+
+            hemisphere_image_urls.append(hemisphere)
+            browser.back()
+        except:
+            return None
+
+        
+
+    return hemisphere_image_urls
+    
+    
 
 
-browser.quit
 
 
-# In[ ]:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
